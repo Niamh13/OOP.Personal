8312242cf2e5d817a906729885a52ebca8c8b9e3
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2023-12-01 10:53:12 +0000

    Updated

--- CoOrdinates.java
+++ CoOrdinates.java
@@ -1,32 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package xpath.belowwaterapp;
+package belowwaterappp;
 
-import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 /**
  *
  * @author niamh
  */
-public class CoOrdinates implements Serializable{
-    private int over, down;
+public class CoOrdinates {
+    
+    Connection myConn;
+    private int over, down, sold, caught;
     private String fact;
-    private String type;
 
     public CoOrdinates() {
         over = 0;
         down = 0;
         fact = "";
+        getConnection();
     }
-
-    public CoOrdinates(int over, int down, String type) {
-        this.over = over;
-        this.down = down;
-        this.type = type;
-    }
-    
     
 
     public void setOver(int over) {
@@ -36,13 +33,84 @@ public class CoOrdinates implements Serializable{
     public void setDown(int down) {
         this.down = down;
     }
+    
+    private void getConnection(){
+        try{
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fishdb", "root", "Endless13");
+        } catch (SQLException ex) {
+            System.out.println("Error Connecting:" + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
 
     public String getFact() {
         return fact;
     }
     
     public void compute(){
-        
+        try {
+            Random rand = new Random();
+            int i = rand.nextInt(8);
+            Statement myStatement = myConn.createStatement();
+            if(over>0 && over<=5){
+                if(down>0 && down<=6){
+                    switch (i) {
+                        case 1 -> {
+                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Makerel' GROUP BY species");
+                            sold = myRs.getInt("sold");
+                            caught = myRs.getInt("caught");
+                            fact = "You caught a MACKEREL! \n Mackerel can live up to 20 years old and are a school fish, who tends to stay above 180 feet. \n" + caught + " mackerel have been logged as caught. \n" + sold +" mackerel was sold from this";
+                        }
+                        case 2 -> {
+                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Codling' GROUP BY species");
+                            sold = myRs.getInt("sold");
+                            caught = myRs.getInt("caught");
+                            fact = "You caught a CODLING! \n There a three different species of Codling and humans are their only natural predators. \n" + caught + " codling have been logged as caught. \n" + sold +" codling was sold from this";
+                        }
+                        case 3 -> {
+                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Whiting' GROUP BY species");
+                            sold = myRs.getInt("sold");
+                            caught = myRs.getInt("caught");
+                            fact = "You caught a WHITING! \n Historically, Whiting has been a cheap source of food for the poor, pet food, and even as a filler in flour. 'Whiting'can refer to certain other species of ray-finned fish \n" + caught + " whiting have been logged as caught. \n" + sold +" whiting was sold from this";
+                        }
+                        case 4 -> {
+                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Pollock' GROUP BY species");
+                            sold = myRs.getInt("sold");
+                            caught = myRs.getInt("caught");
+                            fact = "You caught a POLLoCK! \n Pollack can grow to 105cm with living fo rup to 22 years. Pollock is found in a array of everyday products like fish sticks and McDonald's fillet-O-fish sandwiches. \n" + caught + " pollock have been logged as caught. \n" + sold +" pollock was sold from this";
+                        }
+                        case 5 -> {
+                            fact = "CAREFUL, you almost got a shark. Lucky for you, they were on the hunt for some mackerel";
+                        }
+                        case 6 -> {
+                            fact = "You caught a... Oops, a Seagull stole your fish, guess they were hungry";
+                        }
+                        case 7 -> {
+                            fact = "";
+                        }
+                        case 8 -> {
+                            fact = "Hard Luck, none of the fish were interested in your bait";
+                        }
+                        default -> {
+                            fact = "Hard Luck, none of the fish were interested in your bait";
+                        }
+                    }
+                }
+                else{
+                    fact = "Entered an unusable down CoOrdinate";
+                }
+            }
+            else {
+                fact = "Entered an unusable over CoOrdinate";
+            }
+            
+            
+            
+        }
+        catch (SQLException e){
+            System.out.println("Error: " +e);
+        }
     }
     
 }

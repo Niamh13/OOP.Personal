fc44f26f9ad6ffd3226b4ab72f6ca9545b579f69
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2023-12-10 15:04:09 +0000

    Updated

--- CoOrdinates.java
+++ CoOrdinates.java
@@ -1,28 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package belowwaterappp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import javax.swing.JOptionPane;
 import java.util.Random;
 
 /**
- *
+ * CoOrdinates
  * @author niamh
  */
 public class CoOrdinates {
     
-    Connection myConn;
-    private int over, down, sold, caught;
+    private int over, down;
+    private int cSold, cCaught, pSold, pCaught, wSold, wCaught, mSold, mCaught;
     private String fact;
 
     public CoOrdinates() {
         over = 0;
         down = 0;
+        cSold = 0;
+        cCaught = 0;
+        wSold = 0;
+        wCaught = 0;
+        pSold = 0;
+        pCaught = 0;
+        mSold = 0;
+        mCaught = 0;
         fact = "";
-        getConnection();
     }
     
 
@@ -34,14 +39,36 @@ public class CoOrdinates {
         this.down = down;
     }
     
-    private void getConnection(){
-        try{
-            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fishdb", "root", "Endless13");
-        } catch (SQLException ex) {
-            System.out.println("Error Connecting:" + ex.getMessage());
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+    public void setCSold(int cSold){
+        this.cSold = cSold;
+    }
+    
+    public void setCCaught(int cCaught){
+        this.cCaught = cCaught;
+    }
+    
+    public void setWSold(int wSold){
+        this.wSold = wSold;
+    }
+    
+    public void setWCaught(int wCaught){
+        this.wCaught = wCaught;
+    }
+    
+    public void setPSold(int pSold){
+        this.pSold = pSold;
+    }
+    
+    public void setPCaught(int pCaught){
+        this.pCaught = pCaught;
+    }
+    
+    public void setMSold(int mSold){
+        this.mSold = mSold;
+    }
+    
+    public void setMCaught(int mCaught){
+        this.mCaught = mCaught;
     }
 
     public String getFact() {
@@ -54,50 +81,38 @@ public class CoOrdinates {
             int max = 8;
             Random rand = new Random();
             int i = rand.nextInt(max - min + 1) + min;
-            JOptionPane.showMessageDialog(null, i);
             
-            Statement myStatement = myConn.createStatement();
             if(over>0 && over<=5){
                 if(down>0 && down<=6){
                     switch (i) {
                         case 1 -> {
-                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Makerel' GROUP BY species");
-                            sold = myRs.getInt("sold");
-                            caught = myRs.getInt("caught");
-                            fact = "You caught a MACKEREL! \n Mackerel can live up to 20 years old and are a school fish, who tends to stay above 180 feet. \n" + caught + " mackerel have been logged as caught. \n" + sold +" mackerel was sold from this";
+                           fact = "You caught a MACKEREL!\nMackerel can live up to 20 years old and are a school fish, who tends to stay above 180 feet.\n" + mCaught + " mackerel have been logged as wasted, with \n" + mSold +" mackerel sold.";
                         }
                         case 2 -> {
-                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Codling' GROUP BY species");
-                            sold = myRs.getInt("sold");
-                            caught = myRs.getInt("caught");
-                            fact = "You caught a CODLING! \n There a three different species of Codling and humans are their only natural predators. \n" + caught + " codling have been logged as caught. \n" + sold +" codling was sold from this";
+                            
+                            fact = "You caught a CODLING!\nThere a three different species of Codling and humans are their only natural predators.\n" + cCaught + " codling have been logged as wasted, with \n" + cSold +" codling sold.";
                         }
                         case 3 -> {
-                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Whiting' GROUP BY species");
-                            sold = myRs.getInt("sold");
-                            caught = myRs.getInt("caught");
-                            fact = "You caught a WHITING! \n Historically, Whiting has been a cheap source of food for the poor, pet food, and even as a filler in flour. 'Whiting'can refer to certain other species of ray-finned fish \n" + caught + " whiting have been logged as caught. \n" + sold +" whiting was sold from this";
+                            fact = "You caught a WHITING!\nHistorically, Whiting has been a cheap source of food for the poor, pet food, and even as a filler in flour. 'Whiting'can refer to certain other species of ray-finned fish.\n" + wCaught + " whiting have been logged as wasted, with \n" + wSold +" whiting sold.";
                         }
                         case 4 -> {
-                            ResultSet myRs = myStatement.executeQuery("SELECT SUM(caught) AS caught, SUM(sold) AS sold FROM log_table WHERE species = 'Pollock' GROUP BY species");
-                            sold = myRs.getInt("sold");
-                            caught = myRs.getInt("caught");
-                            fact = "You caught a POLLoCK! \n Pollack can grow to 105cm with living fo rup to 22 years. Pollock is found in a array of everyday products like fish sticks and McDonald's fillet-O-fish sandwiches. \n" + caught + " pollock have been logged as caught. \n" + sold +" pollock was sold from this";
+                            
+                            fact = "You caught a POLLOCK!\nPollock can grow to 105cm with living fo rup to 22 years. Pollock is found in a array of everyday products like fish sticks and McDonald's fillet-O-fish sandwiches.\n" + pCaught + " pollock have been logged as wasted, with \n" + pSold +" pollock sold.";
                         }
                         case 5 -> {
-                            fact = "CAREFUL, you almost got a shark. Lucky for you, they were on the hunt for some mackerel";
+                            fact = "CAREFUL, you almost got a shark. Lucky for you, they were on the hunt for some mackerel.";
                         }
                         case 6 -> {
-                            fact = "You caught a... Oops, a Seagull stole your fish, guess they were hungry";
+                            fact = "You caught a... Oops, a Seagull stole your fish, guess they were hungry.";
                         }
                         case 7 -> {
-                            fact = "CAREFUl, you got a Octupus. \n You better throw it.";
+                            fact = "CAREFUl, you got a Octupus.\nYou better throw it.";
                         }
                         case 8 -> {
-                            fact = "Hard Luck, none of the fish were interested in your bait";
+                            fact = "Hard Luck, none of the fish were interested in your bait.";
                         }
                         default -> {
-                            fact = "Hard Luck, none of the fish were interested in your bait";
+                            fact = "Hard Luck, none of the fish were interested in your bait.";
                         }
                     }
                 }
@@ -112,7 +127,7 @@ public class CoOrdinates {
             
             
         }
-        catch (SQLException e){
+        catch (Exception e){
             System.out.println("Error: " +e);
         }
     }
